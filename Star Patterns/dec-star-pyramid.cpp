74c2686cbd0cfceae2da99b0eaad076065945928
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-01-05 23:29:52 +0530

    added more star patterns

---
+++ Star Patterns/dec-star-pyramid.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+     for(int i = n; i>0; i--){
+        for(int j = i; j>0; j--){
+            cout << "* ";
+        }
+        cout << "\n";
+     } 
+}
--- Star Patterns/reverse-star-pyramid.cpp
+++ Star Patterns/reverse-star-pyramid.cpp
@@ -12,5 +12,3 @@ int main() {
         cout << endl;
     }
 }
-
-
